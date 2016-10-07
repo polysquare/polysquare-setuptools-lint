3adb6a8c4dd8734567d8b349a89fa7988d171d79
Sam Spilsbury <smspillaz@gmail.com>
2016-10-07 08:04:51 +0800

    setup: Depend on style-linter version with comment detection fixes

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(name="polysquare-setuptools-lint",
           "flake8-import-order",
           "flake8-todo",
           "six",
-          "polysquare-generic-file-linter>=0.1.19"
+          "polysquare-generic-file-linter>=0.1.20"
       ] + _ADDITIONAL_LINTERS,
       extras_require={
           "upload": ["setuptools-markdown"]
