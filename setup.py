fc4e2d1848ec13dbd2218dfbc06c71dd720b5063
Travis-CI <travis@travis-ci.org>
2016-10-10 15:55:33 +0000

    Bump version: 0.0.46 -> 0.0.47
    [ci skip]

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ else:
 
 
 setup(name="polysquare-setuptools-lint",
-      version="0.0.46",
+      version="0.0.47",
       description="""Provides a 'polysquarelint' command for setuptools""",
       long_description_markdown_filename="README.md",
       author="Sam Spilsbury",
